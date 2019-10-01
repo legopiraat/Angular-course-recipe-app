0747e6ae5fd915aec33ba2a581bbd67dd7933c4d
Legopiraat <gijs.venne@hotmail.com>
2019-10-01 16:07:24 +0200

    Initial components and layout

--- src/app/recipe-book/recipe.model.ts
+++ src/app/recipe-book/recipe.model.ts
@@ -1,11 +1,5 @@
 export class Recipe {
-    public name: String;
-     public description: String;
-     public imagePath: String;
 
-     constructor(name: String, description: String, imagePath: String) {
-         this.name = name;
-         this.description = description;
-         this.imagePath = imagePath;
+     constructor(public name: String, public description: String, public imagePath: String) { 
      }
 }
